6318024bf2584f31f91a0a15ae0f0743334c6544
Aron Jones <aron.jones@gmail.com>
2014-01-21 11:15:59 -0500

    first commit, contains python module boilerplate

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://travis-ci.org/defrex/django-bootstrap-form-mixin.png)](https://travis-ci.org/defrex/django-bootstrap-form-mixin)
+
+### Django Bootstrap Form Mixin
+
+TODO
+
+#### Getting Started
+
+    $ pip install django-bootstrap-form-mixin
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from setuptools import setup
+import re
+
+
+with open('bootstrap_form/__init__.py', 'r') as init_file:
+    version = re.search(
+        '^__version__ = [\'"]([^\'"]+)[\'"]',
+        init_file.read(),
+        re.MULTILINE,
+    ).group(1)
+
+
+setup(
+    name='django-bootstrap-form-mixin',
+    description=(
+        'An elegant way to add bootstrap markup to Django forms.'
+    ),
+    url='http://github.com/defrex/django-bootstrap-form-mixin/',
+    license='BSD',
+    author='Aron Jones',
+    author_email='aron.jones@gmail.com',
+    packages=['bootstrap_form'],
+    version=version,
+    install_requires=[
+        'Django>=1.5',
+    ],
+)
